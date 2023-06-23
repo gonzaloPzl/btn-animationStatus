0186e1e27b0ce0fae30a5e1ce2309716d88f53ab
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2023-06-23 02:18:30 -0300

    codigo comentado

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-console.log("Hello word");
 
-let btn = document.querySelector(".magicBtn");
-let btnMagicIcon = document.querySelector(".magicBtn--magicIcon");
-let btnLoaderIcon = document.querySelector(".magicBtn--loaderIcon");
-let btnDoneIcon = document.querySelector(".magicBtn--doneIcon");
 
-let btnText = document.querySelector(".magicBtn--text");
+const btn = document.querySelector(".magicBtn"); // botón general
+
+const btnMagicIcon = document.querySelector(".magicBtn--magicIcon"); //icono de varita
+const btnLoaderIcon = document.querySelector(".magicBtn--loaderIcon"); //icono de loader
+const btnDoneIcon = document.querySelector(".magicBtn--doneIcon"); //icono de done
+
+const btnText = document.querySelector(".magicBtn--text"); //texto del botón
+
 
 btn.addEventListener('click', () => {
-  btn.style.pointerEvents = "none";
-  btn.disabled = true;
-  btnText.textContent = "Trabajando...";
 
-  btnMagicIcon.classList.toggle("none");
-  btnLoaderIcon.classList.toggle("none");
-  btn.classList.add("magicBtn--status__loader");
+  // BLOQUE DE EJECUCIÓN AL DAR CLICK (ESTADO LOADING)
+
+  btn.style.pointerEvents = "none"; // se desactiva el cursor pointer
+  btn.disabled = true; // se desactiva el botón para que no se pueda volver a clickear
+  btnText.textContent = "Trabajando..."; // se cambia el texto por Trabajando...
+
+  btnMagicIcon.classList.toggle("none"); // se esconde el icono de la varita
+  btnLoaderIcon.classList.toggle("none"); // se muestra el icono del loader
+  btn.classList.add("magicBtn--status__loader"); // clase de estado de carga
+
+  // BLOQUE DE EJECUCIÓN AL DAR CLICK FIN
 
   
+  // Por motivos de prueba se utiliza timeout para el fetch a la api
 
   setTimeout(() => {
-    btn.style.pointerEvents = "none";
-    console.log("Está en el settimeout");
-    btnLoaderIcon.classList.toggle("none");
-    btnDoneIcon.classList.toggle("none");
 
+    // BLOQUE DE EJECUCIÓN ESTADO DONE
+    btnLoaderIcon.classList.toggle("none"); 
+    btnDoneIcon.classList.toggle("none"); 
 
-    btn.classList.replace("magicBtn--status__loader" ,"magicBtn--status__done")
-    btnText.textContent = "Listo";
+    btn.classList.replace("magicBtn--status__loader" ,"magicBtn--status__done") // clase de estado done
+    btnText.textContent = "Listo"; // se cambia el texto por 'Listo'
+    // BLOQUE DE EJECUCIÓN ESTADO DONE
 
-  }, 2000)
+  }, 4000)
 
 
   setTimeout(() => {
-    btn.style = "";
-    btn.disabled = false;
+    // BLOQUE DE EJECUCIÓN PARA VOLVER AL ESTADO DEFAULT
+    btn.style = ""; // se limpian los estilos 
+    btn.disabled = false; 
     btn.classList.remove("magicBtn--status__done");
-    btnDoneIcon.classList.toggle("none");
+    btnDoneIcon.classList.toggle("none"); 
     btnMagicIcon.classList.toggle("none");
     btnText.textContent = "Mejorar mi texto";
-  }, 4000)
+    // BLOQUE DE EJECUCIÓN PARA VOLVER AL ESTADO DEFAULT FIN
+  }, 6000)
 
 
 
